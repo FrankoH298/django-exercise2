eed370c2043fc41aad1948d1edc7535825bcfc74
FrankoH298 <francoh298@gmail.com>
2020-06-30 09:40:29 -0300

    Agregado /migrations a .gitignore, agregado models (Direccion, Proveedor)

--- core/sistema/admin.py
+++ core/sistema/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from sistema.models import Proveedor, Direccion
 
 # Register your models here.
+
+admin.site.register(Direccion,)
+admin.site.register(Proveedor,)
--- core/sistema/models.py
+++ core/sistema/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Direccion(models.Model):
+    id = models.AutoField(primary_key=True)
+    Calle = models.CharField(max_length=50)
+    Numero = models.PositiveIntegerField()
+    Comuna = models.CharField(max_length=5)
+    Ciudad = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return ("ID: {} | {} {} | {}".format(self.id, self.Calle, self.Numero, self.Ciudad))
+        
+    class Meta:
+        verbose_name = "Direccion"
+        verbose_name_plural = "Direcciones"
+
+class Proveedor(models.Model):
+    RUT = models.PositiveIntegerField(primary_key=True)
+    Nombre = models.CharField(max_length=15)
+    Direccion = models.ForeignKey('Direccion', on_delete = models.CASCADE,)
+    Telefono = models.BigIntegerField()
+    WEB = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return ("RUT: {} | {} | Telefono: {}".format(self.RUT, self.Nombre, self.Telefono))
+        
+    class Meta:
+        verbose_name = "Proveedor"
+        verbose_name_plural = "Proveedores"
