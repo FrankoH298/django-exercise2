05004415344245d8dc4c865df725da6827b7e6de
FrankoH298 <francoh298@gmail.com>
2020-07-01 15:02:02 -0300

    Agregado clases faltantes

--- core/sistema/admin.py
+++ core/sistema/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from sistema.models import Proveedor, Direccion, Cliente
+from sistema.models import *
 
 # Register your models here.
 
 admin.site.register(Direccion,)
 admin.site.register(Proveedor,)
 admin.site.register(Cliente,)
+admin.site.register(Categoria,)
+admin.site.register(Venta,)
+admin.site.register(Producto,)
--- core/sistema/models.py
+++ core/sistema/models.py
@@ -44,3 +44,44 @@ class Cliente(models.Model):
     class Meta:
         verbose_name = "Cliente"
         verbose_name_plural = "Clientes"
+
+
+class Categoria(models.Model):
+    ID = models.PositiveIntegerField(primary_key=True)
+    Nombre = models.CharField(max_length=15)
+    Descripcion = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return ("ID: {} | {} | Descripcion: {}".format(self.ID, self.Nombre, self.Descripcion))
+        
+    class Meta:
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
+
+
+class Producto(models.Model):
+    ID = models.PositiveIntegerField(primary_key=True)
+    Nombre = models.CharField(max_length=15)
+    Precio = models.PositiveIntegerField()
+    Stock = models.IntegerField()
+    
+    def __str__(self):
+        return ("ID: {} | {}".format(self.ID, self.Nombre))
+        
+    class Meta:
+        verbose_name = "Producto"
+        verbose_name_plural = "Productos"
+
+
+class Venta(models.Model):
+    ID = models.PositiveIntegerField(primary_key=True)
+    Fecha = models.DateField()
+    Monto_Final = models.IntegerField()
+    Descuento = models.IntegerField()
+    
+    def __str__(self):
+        return ("ID: {} | {}".format(self.ID, self.Nombre))
+        
+    class Meta:
+        verbose_name = "Venta"
+        verbose_name_plural = "Ventas"
